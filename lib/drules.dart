d05d0113b4aff4861f124b34f99924576fec2494
camrongiuliani <cgiuliani@savanainc.com>
2025-01-09 08:31:49 -0500

    chore: expose CustomizableMemberAccessor

--- lib/drules.dart
+++ lib/drules.dart
@@ -9,3 +9,4 @@ export 'src/rule.dart' hide ruleId;
 export 'src/repos/index.dart';
 export 'src/events/activation.dart';
 export 'src/rule_context.dart' hide Facts;
+export 'package:template_expressions/template_expressions.dart' show CustomizableMemberAccessor;
